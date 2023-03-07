9fdba7ccdc436a9a91f203d101ea634c3689d41b
Bichen Wang <bichenwang@gmail.com>
2023-03-07 14:11:51 -0800

    [HB-5107] AdMob to 21.5.0

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.21.4.0.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.21.5.0.0"
 
         buildConfigField(
             "String",
@@ -74,7 +74,7 @@ dependencies {
     "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.+")
 
     // Partner SDK
-    implementation("com.google.android.gms:play-services-ads:21.4.0")
+    implementation("com.google.android.gms:play-services-ads:21.5.0")
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")

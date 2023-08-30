1586192411821d853cfe72f143827dd1f2172237
vu <vu.chau@chartboost.com>
2023-08-30 15:45:55 -0400

    AdMob - 4.22.1.0.1

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.22.1.0.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.22.1.0.1"
 
         buildConfigField(
             "String",

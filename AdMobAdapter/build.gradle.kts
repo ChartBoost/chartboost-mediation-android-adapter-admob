e0dbc0e5a2a416b2d18852db482e0ca32d00aaf7
Ray Graham <rayg@bitwisedesign.com>
2024-05-29 08:55:21 -0700

    Explicitely define JDK 17 in build.gradle.kts

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -65,6 +65,15 @@ android {
         viewBinding = true
         buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {

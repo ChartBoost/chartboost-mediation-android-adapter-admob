4ca14c155bff1b98cd1b97ff34184a140ae30c06
Ray Graham <rayg@bitwisedesign.com>
2024-05-29 08:58:37 -0700

    Revert "Explicitely define JDK 17 in build.gradle.kts"
    This reverts commit e0dbc0e5a2a416b2d18852db482e0ca32d00aaf7.

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -65,15 +65,6 @@ android {
         viewBinding = true
         buildConfig = true
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {

f267e2c780d6bfdf31f36054cbee34b8d5a5b884
Bichen Wang <bichenwang@gmail.com>
2024-12-13 04:45:28 -0800

    compile33

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -35,11 +35,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.admobadapter"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.23.3.0.0"
 
@@ -93,7 +93,7 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
-    implementation("com.google.android.gms:play-services-ads:23.3.0")
+    implementation("com.google.android.gms:play-services-ads:22.3.0")
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")

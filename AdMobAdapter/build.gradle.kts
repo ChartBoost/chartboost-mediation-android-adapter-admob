a8b38a02e6286ca2a994c0754d5da1ffcceb1fc0
Ray Graham <rayg@bitwisedesign.com>
2025-07-15 07:56:35 -0700

    Bumped minSdk and Kotlin

--- AdMobAdapter/build.gradle.kts
+++ AdMobAdapter/build.gradle.kts
@@ -38,7 +38,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.24.5.0.0"
